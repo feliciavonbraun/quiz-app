706bee021237143158074344b91d1e95aa49c63c
Oliver Nygren <oliver.nygren@icloud.com>
2021-01-26 11:45:39 +0100

    working bot code - first version

--- JS/bot.js
+++ JS/bot.js
@@ -1,14 +1,19 @@
 let botGuess = Math.floor(Math.random() * 20) + 1;
 
 
-
 class Bot{
     constructor(speed, guess){
         this.speed = speed;
         this.guess = guess;
     }
+
+    speed() {
+      this.speed = 2000;
+    }
 }
 
-const botOne = new Bot(2000,botguess)
+const botOne = new Bot(2000,botGuess)
 
-console.log(botOne);
+setTimeout(() => { 
+  console.log(botOne.guess);
+}, botOne.speed);
