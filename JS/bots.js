476b0479ec228a82e277311bd441fd2be6aab23b
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-01-27 12:02:21 +0100

    functionality for bots guess depening on each other

--- JS/bots.js
+++ JS/bots.js
@@ -1,17 +1,25 @@
 
 //-------Bot-------
+let firstAnswer;
+let secondAnswer;
+let thirdAnswer;
+let answerMM = 12;
+
+
+// const botList = ['botOne', 'botTwo', 'botThree'];
+
+// for(let i = 0; i < botList.length; i++){
 
 const botOne = setTimeout(() => {
-
+    
     function bot1answer(){
-
-        let firstAnswer = Math.floor(Math.random() * 20) + 1;
+         firstAnswer = Math.floor(Math.random() * 20) + 1;
          document.getElementById("bot1Answer").innerHTML = firstAnswer;
          return firstAnswer;
      }
     
     console.log(bot1answer());
-    numberOfGuesses = 0;
+    numberOfGuesses++;
     
 }, 2000);
 
@@ -20,14 +28,24 @@ const botTwo = setTimeout(() => {
 
     function bot2answer(){
 
-        let secondAnswer = Math.floor(Math.random() * 20) + 1;
-         document.getElementById("bot2Answer").innerHTML = secondAnswer;
-         return secondAnswer;
+        if(answerMM == firstAnswer) {
+            console.log('bot1 won')
+        }
+        else if(answerMM < firstAnswer){
+            console.log('lower');
+            secondAnswer = Math.floor(Math.random() * answerMM - (firstAnswer)) + 1;
+            document.getElementById("bot2Answer").innerHTML = secondAnswer;
+            return secondAnswer;
+        } else {
+            console.log('higher');
+            secondAnswer = Math.floor(Math.random() * answerMM + (firstAnswer)) + 1;
+            document.getElementById("bot2Answer").innerHTML = secondAnswer;
+            return secondAnswer;
+        }
      }
-
     
     console.log(bot2answer());
-    numberOfGuesses = 0;
+    numberOfGuesses++;
     
 }, 3000);
 
@@ -36,13 +54,25 @@ const botThree = setTimeout(() => {
 
     function bot3answer(){
 
-        let thirdAnswer = Math.floor(Math.random() * 20) + 1;
-         document.getElementById("bot3Answer").innerHTML = thirdAnswer;
-         return thirdAnswer;
+        if(answerMM == secondAnswer){
+            console.log('bot2 won');
+        }
+        else if(answerMM < secondAnswer){
+            console.log('lower');
+            thirdAnswer = Math.floor(Math.random() * answerMM - (secondAnswer)) + 1;
+            document.getElementById("bot3Answer").innerHTML = thirdAnswer;
+            return thirdAnswer;
+        } else {
+            console.log('higher');
+            thirdAnswer = Math.floor(Math.random() * answerMM + (secondAnswer)) + 1;
+            document.getElementById("bot3Answer").innerHTML = thirdAnswer;
+            return thirdAnswer;
+        }
      }
-
     
     console.log(bot3answer());
-    numberOfGuesses = 0;
+    numberOfGuesses++;
     
 }, 5000);
+
+// };
