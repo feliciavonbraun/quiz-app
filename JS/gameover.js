90b2cca83ca8375d46a91a314381115b94155dd4
jv98 <jacob@vahlen.se>
2021-02-01 13:35:34 +0100

    s

--- JS/gameover.js
+++ JS/gameover.js
@@ -1,9 +1,7 @@
 document.getElementById("endscore").innerHTML = localStorage.getItem("createUsername");
 
 function score() {
-    localStorage.key(createUsername)
-    localStorage.key (userScore)
-   
+    
   }
 
 
